b77f8292a86dffcea22e0d98f114c1617201562d
João Pedro Maturo Sofalo <102874668+joao-maturo@users.noreply.github.com>
2022-06-13 13:13:25 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,22 @@
+# mais variaveis de impressão
+nome = 'joão'
+idade = '18'
+sobrenome = 'maturo'
+caracteristica_fisica = 'magro'
+cor_do_olho = 'verde'
+tamanho_dos_dedos = 'gigantes'
+numero_que_calça = '42'
+tamanho_da_camiseta = 'p'
+gostos = 'filmes é jogos'
+filmes = 'de ação'
+cor_do_cabelo = 'preto'
+
+print(f'meu nome é {nome}')
+print(f'minha idade é {idade}')
+print(f'meu sobrenome é {sobrenome}')
+print(f'minhas caracteristicas fisicas são {caracteristica_fisica}')
+print(f'a cor do meu olho é {cor_do_olho}')
+print(f'o tamanho dos meus dedos é {tamanho_dos_dedos}')
+print(f'tamanho da camiseta é {tamanho_da_camiseta} ')
+print(f'gosto muito {gostos}')
+print(f'a cor do meu cabelo é {cor_do_cabelo}')
